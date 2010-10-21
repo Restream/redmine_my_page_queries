1e08f1ba4ce1fb76e117e8a653653f79139b8d08
alvila <milan.stastny@alvila.com>
2010-10-21 13:55:17 +0200

    first commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_my_page_queries do
+  name 'MyPage custom queries'
+  author 'Milan Stastny of ALVILA SYSTEMS'
+  description 'Adds custom queries onto My Page screen'
+  version '0.0.1'
+  author_url 'http://www.alvila.com'
+end
+
