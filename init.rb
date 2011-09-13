8acee68dc1c921d7c074b88b283d5d1dcc5a22c7
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2011-09-13 18:59:02 +0400

    Do not fall in case of negative limit values.

--- init.rb
+++ init.rb
@@ -1,5 +1,13 @@
 require 'redmine'
 
+class String
+  def to_custom_query_limit
+    r = to_i
+    return 1  if r <= 0
+    r
+  end
+end
+
 Redmine::Plugin.register :redmine_my_page_queries do
   name 'MyPage custom queries'
   author 'Milan Stastny of ALVILA SYSTEMS'
